faf9290d70b805c8313c1bad27ca74bffa7ef44f
Luis Alfredo <a01329646@tec.mx>
2022-03-27 15:59:37 -0600

    imagenextra

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -26,3 +26,38 @@ function dividir() {
 	let res = num1 / num2;
 	result.innerHTML = "El resultado es: "+res;
 }
+function sqrt() {
+	var result = document.getElementById("result");
+	var num1 =  document.getElementById("num1").value;
+	var num2 =  document.getElementById("num2").value;
+	let res = Math.sqrt(num1);
+	result.innerHTML = "El resultado es: "+res;
+}
+function exp() {
+	var result = document.getElementById("result");
+	var num1 =  document.getElementById("num1").value;
+	var num2 =  document.getElementById("num2").value;
+	let res = Math.pow(num1, num2);
+	result.innerHTML = "El resultado es: "+res;
+}
+function sin() {
+	var result = document.getElementById("result");
+	var num1 =  document.getElementById("num1").value;
+	var num2 =  document.getElementById("num2").value;
+	let res = Math.sin(num1);
+	result.innerHTML = "El resultado es: "+res;
+}
+function cos() {
+	var result = document.getElementById("result");
+	var num1 =  document.getElementById("num1").value;
+	var num2 =  document.getElementById("num2").value;
+	let res = Math.cos(num1);
+	result.innerHTML = "El resultado es: "+res;
+}
+function tan() {
+	var result = document.getElementById("result");
+	var num1 =  document.getElementById("num1").value;
+	var num2 =  document.getElementById("num2").value;
+	let res = Math.tan(num1);
+	result.innerHTML = "El resultado es: "+res;
+}
